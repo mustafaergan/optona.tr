5834c892c431763aed82f3ff2fcf5daf3e7cf3b8
Mustafa ERGAN <mustafaergan@gmail.com>
2026-03-22 01:58:22 +0300

    Create src/app/cozumler/page.tsx file

---
+++ src/app/cozumler/page.tsx
@@ -0,0 +1,13 @@
+// This is the Cozumler page component.
+import React from 'react';
+
+const CozumlerPage = () => {
+    return (
+        <div>
+            <h1>Cozumler Page</h1>
+            <p>This is the content of the Cozumler page.</p>
+        </div>
+    );
+};
+
+export default CozumlerPage;
